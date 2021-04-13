082879754143c3ee54018418570aa853407af321
jianjiazhang <54599274+Jianjiazhang@users.noreply.github.com>
2021-04-13 15:48:43 -0500

    Update preProcessData.py

--- preProcessData.py
+++ preProcessData.py
@@ -1,58 +1,129 @@
 # import json
 import numpy as np
 import jsonlines
-#import emoji
 import nltk
 import string
 from nltk.corpus import stopwords
 nltk.download('stopwords')
 nltk.download('punkt')
+nltk.download('wordnet')
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
-# for stem words
+from nltk.stem import WordNetLemmatizer
+# for preProcessingString
+import re
+from autocorrect import spell
+# for translate
+import goslate
+from deep_translator import GoogleTranslator
+# for write the file
+import csv
+
 
 # genrate stop-word dictionary inorder to remove useless words and charaters
 stop_words = stopwords.words('english')
-# @USER can't remove
-stop_words.append('@USER')
-stop_words = set(stop_words)
-exclude = set(string.punctuation)
-stop_words = stop_words.union(stop_words, exclude)
-stop_words = list(stop_words)
+# stop_words = set(stop_words)
+# exclude = set(string.punctuation)
+# stop_words = stop_words.union(stop_words, exclude)
+# stop_words = list(stop_words)
 
+RE_EMOJI = re.compile('[\U00010000-\U0010ffff]', flags=re.UNICODE)
 dataStep1 = []
+dataStep2 = []
 ps = PorterStemmer()
-with jsonlines.open('data/train.jsonl') as reader:
+
+# pre precess string
+def preProcessingString(text):
+    # remove emoji
+    # text = RE_EMOJI.sub(r'', text)
+    # lower
+    text = ' '.join([word.lower() for word in word_tokenize(text)])
+
+    # to delete the word like "PAN-00049042"
+    # text = text.split("-")
+    # text = ' '.join(str for str in text)
+
+    # remove numbers, punctuation, strange character
+    text = re.sub("[^a-zA-Z ]+", "", text)
+    # stem and remove words in stopwords
+    
+    text = text.split()
+    text = [ps.stem(word) for word in text if word not in stop_words]
+    text = ' '.join(text)
+    lemmatizer = WordNetLemmatizer()
+    text = lemmatizer.lemmatize(text)
+    # spell check: correct the words like 'wrld' to 'world' (note: take longer time)
+    # text = [spell(word) for word in (nltk.word_tokenize(text))]
+    # text = ' '.join(text)
+    return text
+
+
+
+def getStringBasedOnStringOrList(text):
+    if (text is None):
+      return " "
+    gs = goslate.Goslate()
+    if (type(text) == list):
+        text = ' '.join(str for str in text)
+    # translate when needed
+    # text = GoogleTranslator(source='auto', target='en').translate(text)
+    # language_id = gs.detect(text)
+    # if (language_id == 'German'):
+    #     text = gs.translate(text, "en")
+    text = preProcessingString(text)
+    return text
+
+i = 0
+# get a n * 4 matrix from dataset
+with jsonlines.open('../datasets/dataset.jsonl') as reader:
     for item in reader:
-        label = item["label"]
-        # store string data into temp object
-        temp = item["response"].split()
-        # emoji.demojize(temp)
-        temp = [ps.stem(word) for word in temp if word.lower() not in stop_words]
-        response = ' '.join(temp).lower()
-        # store string data into temp object
-        temp = item["context"][0].split()
-        #emoji.demojize(temp)
-        temp = [ps.stem(word)for word in temp if word.lower() not in stop_words]
-        context = ' '.join(temp).lower()
-        dataStep1.append([label,response,context])
-# build the vocabulary
-vocabulary = set()
-for eachLineAsList in dataStep1:
-    vocabulary = vocabulary.union(eachLineAsList[1].split())
-vocabulary = list(vocabulary)
-#vocabulary size
-d = len(vocabulary)
-# print(d)
-n = 5000
+        i = i + 1
+        # if (i > 100):
+        #     break
+        print("current: ", i)
+        doc_id = item["id"]
+        print(doc_id)
+        print(item["title"])
+        title = getStringBasedOnStringOrList(item["title"])
+        abstract = getStringBasedOnStringOrList(item["abstract"])
+        topic = getStringBasedOnStringOrList(item["topic"])
+        dataStep1.append([doc_id, title, abstract, topic])
+        print(dataStep1[len(dataStep1) - 1])
 
-term_doc_matrix = np.zeros((n, d))
-for i in range(n):
-    for j in range(d):
-        term_doc_matrix[i][j] = dataStep1[i][1].count(vocabulary[j])
 
-print(term_doc_matrix.shape)
 
-# count each words for each line
-# csv
-# np.savetxt("term_doc_matrix.csv", term_doc_matrix)
+i = 0
+# get a n * 4 matrix from publication
+with jsonlines.open('../documents/publication.jsonl') as reader:
+    for item in reader:
+        i = i + 1
+        # if (i > 100):
+        #     break
+        print("current: ", i)
+        doc_id = item["id"]
+        print(doc_id)
+        print(item["title"])
+        title = getStringBasedOnStringOrList(item["title"])
+        abstract = getStringBasedOnStringOrList(item["abstract"])
+        topic = getStringBasedOnStringOrList(item["topic"])
+        dataStep1.append([doc_id, title, abstract, topic])
+        print(dataStep1[len(dataStep1) - 1])
+
+
+print('good till write file')
+with open('dataStep1_translate.csv', 'w') as f:
+  write = csv.writer(f)
+  write.writerows(dataStep1)
+
+
+
+
+# save dataset as csv file 
+dataStep2 = np.asarray(dataStep1)
+# print(dataStep1)
+print(dataStep2.shape)
+# dataStep1Test = np.asarray(dataStep1Test)
+np.savetxt("dataStep2.csv", dataStep2, delimiter = ',', fmt = '%s')
+# np.savetxt("dataStep1Test.csv", dataStep1Test, delimiter = ',',fmt = '%s')
+
+
